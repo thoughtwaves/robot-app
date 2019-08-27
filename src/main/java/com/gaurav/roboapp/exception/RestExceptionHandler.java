0a5a74f1a022cd6078c1079e32b2e1e216415cd7
thoughtwaves <gchauhan90@gmail.com>
2019-08-27 10:33:05 +0530

    RestExceptionHandler

---
+++ src/main/java/com/gaurav/roboapp/exception/RestExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.gaurav.roboapp.exception;
+
+import java.nio.file.AccessDeniedException;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+ 
+    @ExceptionHandler({ OverweightException.class })
+    public ResponseEntity<Object> handleAccessDeniedException(
+      Exception ex, WebRequest request) {
+        return new ResponseEntity<Object>(
+          ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    }
+     
+}
